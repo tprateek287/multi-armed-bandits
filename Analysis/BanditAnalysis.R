f1c11b05de60ff4d3326a02c60a4bbd58877fb9b
Harkar Talwar <harkar.talwar@gmail.com>
2019-03-21 13:33:37 -0700

    Add code comments

--- Analysis/BanditAnalysis.R
+++ Analysis/BanditAnalysis.R
@@ -14,11 +14,16 @@ colnames(df)[colnames(df)=="Cumulative.Reward"] <- "Rewards"
 # Exploration and summary statistics
 library(plyr)
 summary(df)
-plot(Rewards ~ Algorithm, data = df)
 ddply(df, ~ Algorithm, function(data) summary(data$Rewards))
 ddply(df, ~ Algorithm * Time, function(data) summary(data$Rewards))
+# Differences in rewards distribution by algorithm
+plot(Rewards ~ Algorithm, data = df)
+# Rewards histogram at a given time step for each algorithm
+hist(df[df$Algorithm == 'Epsilon Greedy' & df$Time == 600,]$Rewards, breaks = 15)
+hist(df[df$Algorithm == 'UCB' & df$Time == 600,]$Rewards, breaks = 15)
+hist(df[df$Algorithm == 'Thompson Sampling' & df$Time == 600,]$Rewards, breaks = 15)
 
-# Test for normality
+# Shapiro-Wilk Test for normality
 for (time in unique(df$Time)) {
   for (algorithm in unique(df$Algorithm)) {
     gof = shapiro.test(df[df$Algorithm == algorithm & df$Time == time,]$Rewards)
@@ -26,16 +31,18 @@ for (time in unique(df$Time)) {
   }
 }
 
-# set sum-to-zero contrasts for the Anova call
+# Set sum-to-zero contrasts for the Anova call
 contrasts(df$Algorithm) <- "contr.sum"
 
-# LMM test on rewards
+# Linear Mixed Model on rewards. Random intercepts for each simulation nested within
+# the levels of algorithm
 library(lme4)
 library(lmerTest)
 m = lmer(Rewards ~ (Algorithm * Time) + (1|Algorithm:Simulation), data=df)
 Anova(m, type=3, test.statistic="F")
 
-# Post hoc tests
-library(multcomp) # for glht
-library(lsmeans) # for lsm
+# Post hoc tests to check for differences in rewards for each pair of algorithms
+# at the average time interval
+library(multcomp)
+library(lsmeans)
 summary(glht(m, lsm(pairwise ~ Algorithm * Time)), test=adjusted(type="holm"))
